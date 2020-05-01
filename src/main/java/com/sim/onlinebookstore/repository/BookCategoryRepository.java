84bd17d5ef4e1760405c3afb8885ab140005f0cc
Mirage <you@example.cominventoory@gmail.com>
2020-05-01 15:42:21 +0000

    ouverride JSON the url

--- src/main/java/com/sim/onlinebookstore/repository/BookCategoryRepository.java
+++ src/main/java/com/sim/onlinebookstore/repository/BookCategoryRepository.java
@@ -3,7 +3,10 @@ package com.sim.onlinebookstore.repository;
 import com.sim.onlinebookstore.entite.BookCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//this ouverride the url JSON 
+@RepositoryRestResource(collectionResourceRel="bookCateogry", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
     
 }
